e7298fbdd783ba312155f7af9295bda2fd136c99
Brady Johnston <brady.johnston@me.com>
2025-01-06 11:17:38 +0800

    more test

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,11 +1,23 @@
 import pytest
 from pathlib import Path
 import bpy
+from databpy.object import ObjectTracker, BlenderObject
+import polars as pl
+import numpy as np
 
 DATA_FOLDER = Path(__file__).parent / "data"
 
 
-def test_import_via_operator():
-    bpy.ops.import_scene.import_csv_polars(
-        filepath=str(DATA_FOLDER / "deno_star_elemens.csv")
-    )
+def test_import_via_operator(snapshot):
+    deno_path = str(DATA_FOLDER / "deno_star_elemens.csv")
+
+    with ObjectTracker() as o:
+        bpy.ops.import_scene.import_csv_polars(filepath=deno_path)
+        bob = BlenderObject(o.latest())
+
+    # created mesh is the size we expect
+    assert len(bob) == 142
+
+    # the data from the mesh is the same as that from the dataframe
+    df = pl.read_csv(deno_path)
+    assert np.allclose(bob.named_attribute("Dino_X"), df["Dino_X"].to_numpy())
